368fd59cb707b1198eefeb0f369f3832039a0958
daviddinnison <david.g.dinnison@gmail.com>
2017-09-19 03:36:26 -0400

    react native elements test working

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Button } from 'react-native-elements';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>WeatherBoss</Text>
+        <Text>WeatherBossssss</Text>
+
+          <Button
+          raised
+          
+          title={`button test`}
+        />
       </View>
     );
   }
