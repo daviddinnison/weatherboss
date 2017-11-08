ef74577c0ab088d5d1f3f4dcac5df9ca41445cce
daviddinnison <david.g.dinnison@gmail.com>
2017-11-07 23:19:50 -0500

    flexbox implementation

--- components/CurrentConditions.js
+++ components/CurrentConditions.js
@@ -3,26 +3,32 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 //react native
-import { Image, StyleSheet, Text,  View } from 'react-native';
+import { Image, StyleSheet, Text, View } from 'react-native';
 
 
 
-export class CurrentConditions extends React.Component {  
+export class CurrentConditions extends React.Component {
   // {<Text>{current.display_location.full}</Text>}
   render() {
     const current = this.props.currentData;
     console.log(this.props.currentData, 'PROPS FROM CURRENT CONDITIONS')
-    
+
     return (
       <View style={styles.currentconditions}>
         <Text>Current weather:</Text>
-        <Text>{current.weather}</Text>
-        <Text>Feels like: {current.feelslike_f}° F</Text>
-        <Image style={{ height: 50, width: 50 }} source={{ uri: `${current.icon_url}` }} />
-        <Text>Precipitation: {current.precip_1hr_in}</Text>
-        <Text>Humidity: {current.relative_humidity}</Text> 
+        <View style={{ flex: 1, flexDirection: 'row' }}>
+          <View>
+            <Text>{current.weather}</Text>
+            <Image style={{ height: 50, width: 50 }} source={{ uri: `${current.icon_url}` }} />
+          </View>
+          <View>
+            <Text>Feels like: {current.feelslike_f}° F</Text>
+            <Text>Precipitation: {current.precip_1hr_in}</Text>
+            <Text>Humidity: {current.relative_humidity}</Text>
+          </View>
+        </View>
         <Text style={styles.lastupdated}>{current.observation_time}</Text>
-    </View>
+      </View>
     );
   }
 }
@@ -30,6 +36,7 @@ export class CurrentConditions extends React.Component {
 const styles = StyleSheet.create({
   currentconditions: {
     alignItems: 'center',
+    alignSelf: 'stretch',
     backgroundColor: 'white',
     marginBottom: 15,
     padding: 10,
@@ -39,10 +46,10 @@ const styles = StyleSheet.create({
   }
 });
 
-const mapStateToProps = function(state){
-    return {
-      currentData: state.currentData
-    }
+const mapStateToProps = function (state) {
+  return {
+    currentData: state.currentData
+  }
 };
 
 export default connect(mapStateToProps)(CurrentConditions);
